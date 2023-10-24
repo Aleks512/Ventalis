d29eb0253ccb7cd4fc27f949afa62094d420d031
Aleks512 <aleksandracichocka999@gmail.com>
2023-10-24 17:49:20 +0200

    users_in_admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Vous pouvez personnaliser l'affichage des champs dans l'interface d'administration
+    list_display = ('email', 'first_name', 'last_name', 'company', 'is_client', 'is_consultant')
+
+# Enregistrez le modèle CustomUser avec la classe personnalisée CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
