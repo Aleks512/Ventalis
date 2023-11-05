24d734634c801fc12094557032dd4d3bfbb94da5
Aleks512 <aleksandracichocka999@gmail.com>
2023-11-05 10:00:01 +0100

    static bars

--- store/urls.py
+++ store/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
 	#Leave as empty string for base url
-	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
-	path('store/', views.checkout, name="store"),
+	path('store/', views.store, name="store"),
 
 ]
