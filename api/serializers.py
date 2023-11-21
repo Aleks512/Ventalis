a108032737cb46e89c8836ddc449366cbdb06169
Aleks512 <aleksandracichocka999@gmail.com>
2023-11-21 22:43:10 +0100

    serializers

---
+++ api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from users.models import NewUser, Consultant, Customer
+from store.models import Product, Order, OrderItem
+from messagerie.models import ThreadModel, MessageModel
+
+class NewUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewUser
+        fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+class Consultant(serializers.ModelSerializer):
+    class Meta:
+        model = Consultant
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class Order(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class ThreadModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ThreadModel
+        fields = '__all__'
+
+class MessageModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageModel
+        fields = '__all__'
+
